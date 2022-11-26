50f93f95b9f2479818e13cfddfcf50dd778e9fc7
bizexo <118649084+jsPracto@users.noreply.github.com>
2022-11-26 17:29:19 +0530

    todolist-0

--- js/todolist.js
+++ js/todolist.js
@@ -1,18 +1,53 @@
-const todoList = [];
+let iValue = document.getElementById('value');
+let addBtn = document.getElementById('add-list');
 
-function addValue (){
-    let iValue = document.getElementById('value').value;
-    todoList.push(iValue);
-    createList(iValue)
+addBtn.addEventListener("click", addvalue, false);
+
+function addvalue(){
+    iValue.value ? createList(iValue.value) : alert('Add value');
 }
 
 function createList (list){
-   const listCreater = document.getElementById("todoAddedList");
-   const createLI = document.createElement('li');
-   listCreater.append(createLI);
-   todoList.filter((tlist, i) => {
-    tlist
-    console.log('test',i)
-    }).length;
-   console.log(createLI);
+
+    // Object for creating element
+    let listCon={
+        createLI : document.createElement('li'),
+        checkbox : document.createElement('input'),
+        textInput : document.createElement('input'),
+        deleteBtn : document.createElement('button'),
+        editBtn : document.createElement('button')
+    }
+   
+    // Main container
+    const listCreater = document.getElementById("todoAddedList");   
+   
+   listCreater.append(listCon.createLI);
+   listCon.createLI.append(listCon.checkbox, listCon.textInput, listCon.editBtn, listCon.deleteBtn);
+   listCon.checkbox.type = 'checkbox';
+   listCon.textInput.value = list;
+   listCon.textInput.classList.add('disable', 'add-list');
+   listCon.deleteBtn.innerHTML = 'Delete';
+   listCon.editBtn.innerHTML = 'Edit';
+   
+   //event
+   listCon.deleteBtn.addEventListener('click', deleteBtnFn);
+   listCon.editBtn.addEventListener('click', editBtnFn);
+   listCon.checkbox.addEventListener('click', checkboxFn);
+
+
+function deleteBtnFn (){
+    listCreater.removeChild(listCon.createLI);
+}
+
+function editBtnFn (){
+    listCon.textInput.classList.remove('disable');
+    listCon.textInput.focus();
+    listCon.textInput.addEventListener('blur', () =>{
+        listCon.textInput.classList.add('disable'); 
+    })
+}
+
+function checkboxFn (){
+    console.log('checkbox button clicked.');
+}
 }
